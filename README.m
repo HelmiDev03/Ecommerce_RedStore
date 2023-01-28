8ca7252235e5ecf02652abbd97fa85d8ab3a9a1b
Helmi Lakhder <102436033+HelmiDev03@users.noreply.github.com>
2023-01-28 23:59:02 +0100

    Update and rename README.md to README.m

---
+++ README.m
@@ -0,0 +1,2 @@
+This was a little practise on Sass ( components , layout ,syntax....) <br>
+Live : https://helmidev03.github.io/Ecommerce_RedStore/
--- README.md
+++
@@ -1,2 +0,0 @@
-This was a little practise on Sass ( components , layout ,syntax....)
-Live : https://helmidev03.github.io/Ecommerce_RedStore/
